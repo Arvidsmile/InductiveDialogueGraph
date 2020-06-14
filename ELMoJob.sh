3d63c5670d34f387fd7b5f7ba8eb762620367323
Arvid Lindström <arvidl@test3.irc.sara.nl.irc.sara.nl>
2020-06-14 17:22:58 +0200

    updated ELMoJob

--- ELMoJob.sh
+++ ELMoJob.sh
@@ -4,11 +4,15 @@
 #SBATCH --nodes=1
 #SBATCH --partition=gpu_short
 #SBATCH --mail-user=arvid.lindstrom@student.uva.nl
-#SBATCH --output=testingELMoGPU.out
+#SBATCH --output=test-ELMoGPUSwDA.out
 
 module purge
 module load 2019
 module load Anaconda3/2018.12
 
 conda activate ELMoenvironment
-srun python 
+
+cp -r $HOME/CSVData "$TMPDIR"
+
+srun python generateELMO.py --dataset SwDA --testing 1 --step_size 100
+cp "$TMPDIR"/ELMo_SwDA.pickle $HOME/ELMoPickled
