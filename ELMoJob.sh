1215e6f966da882ce7999d8724ec72bbb5ca04fe
Arvid Lindström <arvidl@test3.irc.sara.nl.irc.sara.nl>
2020-06-14 18:41:27 +0200

    successfully ran test on SwDA embeddings

--- ELMoJob.sh
+++ ELMoJob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=01:00:00
-#SBATCH --mem=3G
+#SBATCH --mem=10G
 #SBATCH --nodes=1
 #SBATCH --partition=gpu_short
 #SBATCH --mail-user=arvid.lindstrom@student.uva.nl
@@ -10,9 +10,8 @@ module purge
 module load 2019
 module load Anaconda3/2018.12
 
+. /sw/arch/Debian9/EB_production/2019/software/Anaconda3/2018.12/etc/profile.d/conda.sh
 conda activate ELMoenvironment
 
-cp -r $HOME/CSVData "$TMPDIR"
 
 srun python generateELMO.py --dataset SwDA --testing 1 --step_size 100
-cp "$TMPDIR"/ELMo_SwDA.pickle $HOME/ELMoPickled
