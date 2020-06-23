570e56d8102fba38f62976b024de71b73ab67e92
Arvid <Arvid.Lindstrom@gmail.com>
2020-06-23 14:28:15 +0200

    added baseline job script

---
+++ baseline/baseline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=1:00:00
+#SBATCH --mem=20G
+#SBATCH --nodes=1
+#SBATCH --partition=gpu_short
+#SBATCH --mail-user=arvid.lindstrom@student.uva.nl
+#SBATCH --output=slurm_out/MRDA-ELMo.out
+
+module purge
+module load 2019
+module load Anaconda3/2018.12
+
+. /sw/arch/Debian9/EB_production/2019/software/Anaconda3/2018.12/etc/profile.d/conda.sh
+conda activate Stellarenvironment
+
+srun python kerasTest.py --dataset SwDA --kfold 10 --epochs 50
