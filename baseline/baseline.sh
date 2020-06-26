5f6e011324fceb45d74f720b31dca90f7e85b715
Arvid <Arvid.Lindstrom@gmail.com>
2020-06-26 15:19:34 +0200

    trying to solve merge problem related to adding large files

--- baseline/baseline.sh
+++ baseline/baseline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --time=1:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mem=20G
 #SBATCH --nodes=1
-#SBATCH --partition=gpu_short
+#SBATCH --partition=gpu_shared
 #SBATCH --mail-user=arvid.lindstrom@student.uva.nl
-#SBATCH --output=slurm_out/MRDA-ELMo.out
+#SBATCH --output=slurm_output/SwDA-baseline.out
 
 module purge
 module load 2019
@@ -13,4 +13,4 @@ module load Anaconda3/2018.12
 . /sw/arch/Debian9/EB_production/2019/software/Anaconda3/2018.12/etc/profile.d/conda.sh
 conda activate Stellarenvironment
 
-srun python kerasTest.py --dataset SwDA --kfold 10 --epochs 50
+srun python kerasTest.py --dataset SwDA --kfold 10 --epochs 20
