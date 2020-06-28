6301326503b4560ddead23ee8ef22671222a073c
Arvid Lindström <arvidl@test3.irc.sara.nl.irc.sara.nl>
2020-06-28 21:51:10 +0200

    pipeline for experiments finished

--- stellarexperiments/jobstellar.sh
+++ stellarexperiments/jobstellar.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#SBATCH --time=1:00:00
+#SBATCH --time=3:00:00
 #SBATCH --mem=40G
 #SBATCH --nodes=1
-#SBATCH --partition=gpu_short
+#SBATCH --partition=gpu_shared
 #SBATCH --mail-user=arvid.lindstrom@student.uva.nl
-#SBATCH --output=slurm_output
 
 module purge
 module load 2019
@@ -13,12 +12,14 @@ module load Anaconda3/2018.12
 . /sw/arch/Debian9/EB_production/2019/software/Anaconda3/2018.12/etc/profile.d/conda.sh
 conda activate Stellarenvironment
 
-srun python kerasTest.py \
---dataset MANtIS \
---batch_size 32 \
---epochs 20 \
---kfold 10 \
---num_test 20 \
+# Remember to change  learn_rate, num agg-functions (K), in/out samples and classifier layer in code!
+
+srun python main.py \
+--dataset MRDA \
+--batch_size 256 \
+--epochs 10 \
+--kfold 4 \
+--num_test 10 \
 --testing 1 \
 --start_node 0 \
 --aggregator maxpool \
