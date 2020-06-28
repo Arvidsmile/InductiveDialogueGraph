b4535352fa5ce9da928cd5dab32966f5e7b47c2e
Arvid Lindström <arvidl@test3.irc.sara.nl.irc.sara.nl>
2020-06-28 23:04:14 +0200

    have to fix indentation error

--- stellarexperiments/jobstellar.sh
+++ stellarexperiments/jobstellar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=3:00:00
+#SBATCH --time=1:00:00
 #SBATCH --mem=40G
 #SBATCH --nodes=1
-#SBATCH --partition=gpu_shared
+#SBATCH --partition=gpu_short
 #SBATCH --mail-user=arvid.lindstrom@student.uva.nl
 
 module purge
@@ -12,10 +12,8 @@ module load Anaconda3/2018.12
 . /sw/arch/Debian9/EB_production/2019/software/Anaconda3/2018.12/etc/profile.d/conda.sh
 conda activate Stellarenvironment
 
-# Remember to change  learn_rate, num agg-functions (K), in/out samples and classifier layer in code!
-
 srun python main.py \
---dataset MRDA \
+--dataset MANtIS \
 --batch_size 256 \
 --epochs 10 \
 --kfold 4 \
@@ -23,5 +21,5 @@ srun python main.py \
 --testing 1 \
 --start_node 0 \
 --aggregator maxpool \
---model_size A1 \
---expname lisatest
+--model_size c3 \
+--expname finaltest
